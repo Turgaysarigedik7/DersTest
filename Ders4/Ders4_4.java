7c6e8d882875374642a2b6bdb55e5883db11994e
Turgaysarigedik7 <120941697+Turgaysarigedik7@users.noreply.github.com>
2023-03-09 13:11:26 +0300

    Ders4_2_3_4

---
+++ Ders4/Ders4_4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ders4_4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //nested if else kullanimi
+        System.out.println("cinsiyet giriniz");
+        String cinsiyet2 = scan.next();
+        System.out.println("yas giriniz");
+        int yas2 = scan.nextInt();
+
+
+        if (cinsiyet2.equals("erkek")) {
+            if (yas2 > 65) {
+                System.out.println("emekli bir beyefendisiniz");
+            } else System.out.println("emekli bir beyefendi degilsiniz");
+        } else if (cinsiyet2.equals("kadin")) {
+            if (yas2 > 55) {
+                System.out.println("emekli bir hanimefendisiniz");
+            } else System.out.println("emekli bir hanimefendi degilsiniz degilsiniz");
+        } else System.out.println("yanlis bir cinsiyet girdiniz");
+    }
+}
