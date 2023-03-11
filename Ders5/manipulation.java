954f40ae87d743803c7bdb319de3dda4e31c0060
Turgaysarigedik7 <120941697+Turgaysarigedik7@users.noreply.github.com>
2023-03-11 16:46:41 +0300

    Add files via upload

---
+++ Ders5/manipulation.java
@@ -0,0 +1,10 @@
+
+import java.net.Proxy;
+
+public class manipulation {
+    public static void main(String[] args) {
+        String str = "Turgay Sefa Sarigedik";
+        System.out.println(str.charAt(0));
+
+    }
+}
